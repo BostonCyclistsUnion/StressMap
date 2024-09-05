b1588941e6616dcc4332d2bbbacd7bfb616660b7
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2024-09-04 22:27:43 -0400

    allow web server to run with arguments

--- web.py
+++ web.py
@@ -1,13 +1,30 @@
 import http.server
 import socketserver
+import argparse
+import sys
 
-PORT = 8000
 
+parser = argparse.ArgumentParser(
+    description='Fetch and process OSM data into LTS')
+parser.add_argument("-plot", type=str,
+                    help="Local filepath of html file")
+parser.add_argument("-port", type=int,
+                    help="Port to serve plot at")
+
+args = parser.parse_args(sys.argv[1:])
+if args.plot:
+    PLOT = args.plot
+else:
+    PLOT = 'map/local_data.html'
+if args.port:
+    PORT = args.port
+else:
+    PORT = 8000
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def translate_path(self, path):
         if self.path == "/":
-            return 'web/index.html'
+            return PLOT
         if self.path.startswith("/plots"):
             return path[1:]  # strip the prefix slash so it can find the plots directory
         else:
